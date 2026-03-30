a25bab031f6dce9b84c9be1d9c6fd93c592bd7b0
Adrineel-Saha <adrineelsaha@gmail.com>
2026-03-30 12:34:19 +0530

    Added MicroservicesAngularProject and models

---
+++ MicroservicesAngularProject/src/app/models/order-item.ts
@@ -0,0 +1,10 @@
+export class OrderItem {
+    id!: number;
+    productId!: number;
+    quantity!: number;
+    price!: number;
+    orderId!: number;
+    name!: string;
+    description!: string;
+    stock!: number;
+}
---
+++ MicroservicesAngularProject/src/app/models/order.ts
@@ -0,0 +1,7 @@
+export class Order {
+    id!: number;
+    userId!: number;
+    createdAt!: Date;
+    userName!: string;
+    email!: string;
+}
