3bbe366ed86fc6be0442b79ac73f347628e3ccd1
Adrineel-Saha <adrineelsaha@gmail.com>
2026-03-30 14:11:26 +0530

    Added UserService HttpClient Module

---
+++ MicroservicesAngularProject/src/app/services/user.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { User } from '../models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient) { }
+
+  private backendUserURI= "http://localhost:9191/api/users";
+
+  public createUser(user:User){
+    return this.http.post<User>(this.backendUserURI,user);
+  }
+
+  public getAllUsers(){
+    return this.http.get<User[]>(this.backendUserURI);
+  }
+
+  public getUser(userId:number){
+    return this.http.get<User>(this.backendUserURI+"/"+userId);
+  }
+
+  public updateUser(userId:number,user:User){
+    return this.http.put<User>(this.backendUserURI+"/"+userId,user);
+  }
+
+  public deleteUser(userId:number){
+    return this.http.delete<string>(this.backendUserURI+"/"+userId,{responseType:'text' as 'json'});
+  }
+
+}
